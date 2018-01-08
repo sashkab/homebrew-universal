a9d11b333cad6bd6cc0e5cc3a03c5527afc7d3d6
Aleks Bunin <github@compuix.com>
2018-01-08 09:56:27 -0500

    openssl 1.0.2n

--- Formula/uopenssl.rb
+++ Formula/uopenssl.rb
@@ -4,11 +4,11 @@
 class Uopenssl < Formula
   desc "SSL/TLS cryptography library"
   homepage "https://openssl.org/"
-  url "https://www.openssl.org/source/openssl-1.0.2m.tar.gz"
-  mirror "https://dl.bintray.com/homebrew/mirror/openssl-1.0.2m.tar.gz"
-  mirror "https://www.mirrorservice.org/sites/ftp.openssl.org/source/openssl-1.0.2m.tar.gz"
-  mirror "http://artfiles.org/openssl.org/source/openssl-1.0.2m.tar.gz"
-  sha256 "8c6ff15ec6b319b50788f42c7abc2890c08ba5a1cdcd3810eb9092deada37b0f"
+  url "https://www.openssl.org/source/openssl-1.0.2n.tar.gz"
+  mirror "https://dl.bintray.com/homebrew/mirror/openssl-1.0.2n.tar.gz"
+  mirror "https://www.mirrorservice.org/sites/ftp.openssl.org/source/openssl-1.0.2n.tar.gz"
+  mirror "http://artfiles.org/openssl.org/source/openssl-1.0.2n.tar.gz"
+  sha256 "370babb75f278c39e0c50e8c4e7493bc0f18db6867478341a832a982fd15a8fe"
 
   keg_only :provided_by_osx,
     "Apple has deprecated use of OpenSSL in favor of its own TLS and crypto libraries"
